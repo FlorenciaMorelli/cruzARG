28963e0afcc462c5124ee640e0fc0ad526ef4d58
Florencia Morelli <110637670+FlorenciaMorelli@users.noreply.github.com>
2024-01-10 19:13:27 -0300

    [Controlador] feature: creación de vehículos con librería Random
    Con la implementación de las bibliotecas cstdlib y ctime de la librería estándar de C++, utilizamos las funciones rand() y srand() para randomizar la creación de autos/taxis y el espacio entre ellos

--- Trafico.cpp
+++ Trafico.cpp
@@ -1,16 +1,54 @@
 #include "Trafico.h"
 
 #include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <ctime>
+
 #include "DEFINITIONS.h"
 #include <vector>
 
-Trafico::Trafico(float posX, float posY, bool direccion) {
-    vehiculos.push_back(new Taxi(posX, posY, direccion));
-    vehiculos.push_back(new Taxi(vehiculos.back()->getPosOriginalX() + SEPARACION_ENTRE_VEHICULOS, posY, direccion));
-    vehiculos.push_back(new Auto(vehiculos.back()->getPosOriginalX() + SEPARACION_ENTRE_VEHICULOS, posY, direccion));
-    vehiculos.push_back(new Taxi(vehiculos.back()->getPosOriginalX() + SEPARACION_ENTRE_VEHICULOS, posY, direccion));
-    vehiculos.push_back(new Auto(vehiculos.back()->getPosOriginalX() + SEPARACION_ENTRE_VEHICULOS, posY, direccion));
-    vehiculos.push_back(new Auto(vehiculos.back()->getPosOriginalX() + SEPARACION_ENTRE_VEHICULOS, posY, direccion));
+Trafico::Trafico(float tamVentana, float posY, bool direccion) {
+    float posX = 0.0;
+    if(direccion){
+        posX = -ANCHO_AUTO;
+
+        vehiculos.push_back(new Taxi(posX, posY, direccion));
+
+        for (int i = 1; i < 12; ++i) {
+            float nuevaPosicionX = vehiculos.back()->getPosOriginalX() + SEPARACION_ENTRE_VEHICULOS +
+                                   rand() % (int)(SEPARACION_PARA_PERSONAJE - SEPARACION_ENTRE_VEHICULOS + 1);
+
+            if (nuevaPosicionX + ANCHO_AUTO <= tamVentana) {
+                if (rand() % 2 == 0) {
+                    vehiculos.push_back(new Taxi(nuevaPosicionX, posY, direccion));
+                } else {
+                    vehiculos.push_back(new Auto(nuevaPosicionX, posY, direccion));
+                }
+            } else {
+                break;
+            }
+        }
+    } else {
+        posX = tamVentana + ANCHO_TAXI;
+
+        vehiculos.push_back(new Taxi(posX, posY, direccion));
+
+        for (int i = 1; i < 12; ++i) {
+            float nuevaPosicionX = vehiculos.back()->getPosOriginalX() - SEPARACION_ENTRE_VEHICULOS -
+                                   rand() % (int)(SEPARACION_PARA_PERSONAJE - SEPARACION_ENTRE_VEHICULOS + 1);
+
+            if (nuevaPosicionX - ANCHO_AUTO >= 0.0) {
+                if (rand() % 2 == 0) {
+                    vehiculos.push_back(new Taxi(nuevaPosicionX, posY, direccion));
+                } else {
+                    vehiculos.push_back(new Auto(nuevaPosicionX, posY, direccion));
+                }
+            } else {
+                break;
+            }
+        }
+
+    }
 }
 
 void Trafico::move(const sf::Vector2u& tamVentana) {
