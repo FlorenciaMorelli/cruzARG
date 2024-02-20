7ec8965a9a585ead866b79d86e54ab88c5eb8226
Florencia Morelli <florenciamorelliit@gmail.com>
2024-02-20 17:09:21 -0300

    tests: agrego todos los tests

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "tau/tau.h"
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "DEFINITIONS.h"
 #include "Juego.h"
 #include "Personaje.h"
 #include "Mate.h"
+#include "Trafico.h"
+
 
 using namespace sf;
+using namespace std;
+
 
 TAU_MAIN()
 
@@ -13,56 +18,56 @@ TEST(CruzARG, Victoria) {
     Juego juego;
     RenderWindow ventanaTest;
 
-    // TODO:
-    // Simula una situación donde el personaje colisiona con un objeto
-    // Asegúrate de que la función checkCollision() funcione correctamente
-    // y devuelva verdadero cuando hay una colisión
     Personaje personaje(ventanaTest.getSize());
-    personaje.setPosicion(100.0f, 100.0f);
     Mate mate(ventanaTest.getSize());
+
+    personaje.setPosicion(100.0f, 100.0f);
     mate.setPosicion(100.0f, 100.0f);
+
     CHECK(mate.tomado(&personaje, ventanaTest) == true, "Falla mate.tomado()");
+
+    personaje.setPosicion(0.0, 100.0);
+    mate.setPosicion(100.0, 0.0);
+
+    CHECK(mate.tomado(&personaje, ventanaTest) == false, "Falla mate.tomado()");
 }
 
-// Define una prueba para el movimiento del personaje
 TEST(CruzARG, MovimientoDelPersonaje) {
-    Juego juego; // Crea una instancia del juego
-    Personaje personaje; // Crea una instancia del personaje
+    Vector2u tamanioVentana(800, 600);
+    Personaje personaje(tamanioVentana);
 
-    // Simula el movimiento del personaje
-    // Asegúrate de que la función de movimiento del personaje funcione correctamente
-    // y cambie la posición del personaje según la entrada del jugador
-    juego.moverPersonaje(personaje, Movimiento::Arriba);
-    Tau::Expect(personaje.posicion()).toBe(/* posición esperada después del movimiento hacia arriba */);
+    Event eventoArriba, eventoAbajo, eventoIzquierda, eventoDerecha;
+    eventoArriba.key.code = Keyboard::Key::Up;
+    eventoAbajo.key.code = Keyboard::Key::Down;
+    eventoIzquierda.key.code = Keyboard::Key::Left;
+    eventoDerecha.key.code = Keyboard::Key::Right;
 
-    // Repite para otros movimientos (abajo, izquierda, derecha, etc.)
+    personaje.setPosicion(100.0f, 100.0f);
+    personaje.move(eventoArriba, tamanioVentana);
+    CHECK(personaje.getShape().getPosition().x == 100.0f && personaje.getShape().getPosition().y == 100.0f - PERSONAJE_CANT_MOVIMIENTO, "Falla al mover el personaje hacia arriba");
+
+    personaje.setPosicion(100.0f, 100.0f);
+    personaje.move(eventoAbajo, tamanioVentana);
+    CHECK(personaje.getShape().getPosition().x == 100.0f && personaje.getShape().getPosition().y == 100.0f + PERSONAJE_CANT_MOVIMIENTO, "Falla al mover el personaje hacia abajo");
+
+    personaje.setPosicion(100.0f, 100.0f);
+    personaje.move(eventoDerecha, tamanioVentana);
+    CHECK(personaje.getShape().getPosition().x == 100.0f + PERSONAJE_CANT_MOVIMIENTO && personaje.getShape().getPosition().y == 100.0f, "Falla al mover el personaje hacia la derecha");
+
+    personaje.setPosicion(100.0f, 100.0f);
+    personaje.move(eventoIzquierda, tamanioVentana);
+    CHECK(personaje.getShape().getPosition().x == 100.0f - PERSONAJE_CANT_MOVIMIENTO && personaje.getShape().getPosition().y == 100.0f, "Falla al mover el personaje hacia la izquierda");
 }
 
-// Define una prueba para la lógica de triunfo del juego
-TEST(CruzARG, TriunfoDelJuego) {
-    Juego juego; // Crea una instancia del juego
+TEST(CruzARG, Derrota) {
+    RenderWindow ventanaTest;
+    Personaje personaje(ventanaTest.getSize());
+    Trafico trafico(2, ventanaTest.getSize().x, 100.0f, true);
 
-    // Simula una situación donde el personaje alcanza la meta
-    // Asegúrate de que la lógica de triunfo del juego funcione correctamente
-    Tau::Expect(juego.esTriunfo()).toBe(true);
+    personaje.setPosicion(100.0, 100.0);
+    CHECK(trafico.checkCollision(&personaje, ventanaTest) == false, "Falla checkCollision()");
+
+
+    personaje.setPosicion(50.0, 50.0);
+    CHECK(trafico.checkCollision(&personaje, ventanaTest) == false, "Falla checkCollision()");
 }
-
-// Define una prueba para la lógica de derrota del juego
-TEST(CruzARG, DerrotaDelJuego) {
-    Juego juego; // Crea una instancia del juego
-
-    // Simula una situación donde el tiempo se agota o el personaje pierde todas las vidas
-    // Asegúrate de que la lógica de derrota del juego funcione correctamente
-    Tau::Expect(juego.esDerrota()).toBe(true);
-}
-
-// Define una prueba para la lógica de puntuación del juego
-TEST(CruzARG, PuntuacionDelJuego) {
-    Juego juego; // Crea una instancia del juego
-
-    // Simula una situación donde el jugador realiza acciones que otorgan puntos
-    // Asegúrate de que la lógica de puntuación del juego funcione correctamente
-    juego.realizarAccionQueOtorgaPuntos();
-    Tau::Expect(juego.puntuacion()).toBe(/* puntuación esperada después de realizar la acción */);
-}
-
